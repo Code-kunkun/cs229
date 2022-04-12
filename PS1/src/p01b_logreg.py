710817bc94da73bc7e5005c36de7a3f901a32db8
lyk <1601418768@qq.com>
2022-04-12 11:07:04 +0800

    add ps0_1

--- PS1/src/p01b_logreg.py
+++ PS1/src/p01b_logreg.py
@@ -15,6 +15,16 @@ def main(train_path, eval_path, pred_path):
     x_train, y_train = util.load_dataset(train_path, add_intercept=True)
 
     # *** START CODE HERE ***
+    x_eval, y_eval = util.load_dataset(eval_path, add_intercept=True)
+
+    clf = LogisticRegression()
+    clf.fit(x_train, y_train)
+    
+    res = clf.predict(x_eval)
+    
+    accuracy = np.sum((res > 0.5) == y_eval)
+    print("accuracy = %d" %accuracy)
+    np.savetxt(pred_path, res > 0.5, fmt="%d")
     # *** END CODE HERE ***
 
 
@@ -27,6 +37,9 @@ class LogisticRegression(LinearModel):
         > clf.predict(x_eval)
     """
 
+    def sigmoid(self, z):
+            return 1 / (1 + np.exp(-z))
+
     def fit(self, x, y):
         """Run Newton's Method to minimize J(theta) for logistic regression.
 
@@ -35,6 +48,26 @@ class LogisticRegression(LinearModel):
             y: Training example labels. Shape (m,).
         """
         # *** START CODE HERE ***
+
+        m, n = x.shape
+        self.theta = np.zeros(n)
+        
+        # Newton's method
+        while True:
+            theta_old = np.copy(self.theta)
+
+            h_x = self.sigmoid(x.dot(self.theta))
+            H = (x.T * h_x * (1 - h_x)).dot(x) / m 
+            gradient_J_theta = x.T.dot(h_x - y) / m 
+
+            # update theta
+            self.theta -= np.linalg.inv(H).dot(gradient_J_theta) # np.linalg.inv: 矩阵求逆
+
+            # End training
+            if np.linalg.norm(self.theta - theta_old, ord=1) < self.eps: # 以第一范式为判定原则
+                break
+
+
         # *** END CODE HERE ***
 
     def predict(self, x):
@@ -47,4 +80,5 @@ class LogisticRegression(LinearModel):
             Outputs of shape (m,).
         """
         # *** START CODE HERE ***
+        return self.sigmoid(x.dot(self.theta))
         # *** END CODE HERE ***
